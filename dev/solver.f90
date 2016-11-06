856d12a24d2158c72afc84c0830958ad2b544953
Jonathan Braden <jonathan.n.braden@gmail.com>
2016-11-06 16:22:39 +0000

    Minor fixes to solver.  Compiles

--- dev/solver.f90
+++ dev/solver.f90
@@ -11,11 +11,11 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 module Nonlinear_Solver
   use constants
-  use Model
+!  use Model
   implicit none
   
   type Solver
-     integer :: nVar
+     integer :: nVar, u
      integer :: maxIter; real(dl) :: kick_param  ! Adjustable solver parameters
      real(dl), dimension(:,:), allocatable :: L, L_const  ! Linear operator
      real(dl), dimension(:), allocatable :: S    ! Source term
@@ -25,12 +25,21 @@ module Nonlinear_Solver
      real(dl), dimension(:), allocatable :: f_prev
      logical :: spec_space  ! whether or not we work in spectral space (not yet used)
   end type Solver
+
+!  abstract interface
+!     subroutine src(fld,eom)
+!       real(dl), dimension(:), intent(in) :: fld
+!       real(dl), dimension(:), intent(out) :: eom
+!     end subroutine src
+!  end interface
   
 contains
 
-  subroutine intialise_solver(this,n)
+  subroutine intialise_solver(this,n, nit, kick)
     type(Solver), intent(inout) :: this
-    this%nVar = n; maxIter = 100; kick_param = 0.1_dl
+    integer, intent(in) :: n,nit
+    real(dl), intent(in) :: kick
+    this%nVar = n; this%maxIter = 100; this%kick_param = 0.1_dl
     allocate(this%L(1:n,1:n),this%L_const(1:n,1:n)); allocate(this%S(1:n))
     allocate(this%del(1:n)); allocate(this%f_prev(1:n))
     allocate(this%ipiv(1:n))
@@ -40,12 +49,13 @@ contains
     type(Solver), intent(inout) :: this
     this%nVar = -1
     deallocate(this%L, this%L_const, this%S)
-    deallocate(this%res, this%del)
+    deallocate(this%del, this%f_prev)
     deallocate(this%ipiv)
   end subroutine delete_solver
 
   subroutine solve(this,f_cur)
     type(Solver), intent(inout) :: this
+    real(dl), dimension(:), intent(inout) :: f_cur
     integer :: i
     
     do i=1,this%maxIter
@@ -77,19 +87,20 @@ contains
     real(dl), dimension(:), intent(inout) :: f_cur
     integer :: i, info
     real(dl) :: alpha, err_rms, err_max, res
-
+    real(dl) :: b1  ! fix this and remove it
+    
     ! Use the equations of motion to fill the Linear and Source term
     ! call fill_equations(this%L,this%S,f_cur) ! write this.  Currently in model file
     ! Do source and linear part separately
     !
     !!!! These should be moved into a separate file.  For now I'm storing them in here
-    this%L(:,:) = l0(:,:)  ! Put in the linear piece
-    do i=1,this%nVar
-       L(i,i) = L(i,i) - vdprime(phi)
-    enddo
+    !this%L(:,:) = l0(:,:)  ! Put in the linear piece
+    !do i=1,this%nVar
+    !   L(i,i) = L(i,i) - vdprime(phi)
+    !enddo
     ! The stuff above this is completely broken right now
 
-    res = sqrt(sum(this%S*this%S))  ! Current residual
+    res = sqrt(sum(this%S**2))  ! Current residual
     ! Compute the required perturbation
     call DGESV(this%nVar,1,this%L,this%ipiv,this%S,this%nVar,info)
     this%del = this%S
@@ -99,12 +110,12 @@ contains
     endif
 
     ! Why am I ever setting this thing here?
-    b1 = res + kick_param ! If we're not converging, this allows us to kick ourselves
+    b1 = res + this%kick_param ! If we're not converging, this allows us to kick ourselves
     alpha = 2._dl
     do i=1,8
        alpha = alpha/2._dl
        this%f_prev = f_cur + alpha*this%del
-       this%S = source(this%f_prev) ! replace with a call to source
+       this%S = 0._dl !source(this%f_prev) ! replace with a call to source
        this%S = this%S
        err_max = maxval(abs(this%S))
        err_rms = sqrt(sum(this%S**2))
@@ -113,7 +124,7 @@ contains
 
     this%f_prev = f_cur            ! Store previous iteration
     f_cur = f_cur + alpha*this%del  ! Update function
-    this%S = source(f_cur)          ! Store violation of the equation of motion
+    this%S = 0._dl !source(f_cur)          ! Store violation of the equation of motion
   end subroutine line_iteration
 
   !>@brief
@@ -149,6 +160,21 @@ contains
   !> convergence of our iterator
   subroutine solver_output(this)
     type(Solver), intent(in) :: this
+    integer :: i
+
+    do i=1,this%nVar
+       write(this%u,*) 
+    enddo
+    write(this%u,*)
   end subroutine solver_output
+
+!!!!!!
+! Temporary inclusion here before factoring into a separate file
+!!!!!!
+ ! subroutine source(fld,eom)
+ !   real(dl), intent(in) :: fld
+ !   real(dl), intent(out) :: eom
+ !   integer :: i  
+ ! end subroutine source
   
 end module Nonlinear_Solver
