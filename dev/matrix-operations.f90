77694592db6f05bed23e0c6a93344ac3204cb3bd
Jonathan Braden <jonathan.n.braden@gmail.com>
2016-11-09 16:06:29 +0000

    Added chebyshev and matrix-operations for pseudospec work

---
+++ dev/matrix-operations.f90
@@ -0,0 +1,63 @@
+module Matrix
+  use constants
+  implicit none
+  
+contains
+
+  !>@brief
+  !> Invert an NxN matrix
+  !>
+  !>@param[in]   A: real(dl) - nxn matrix to invert
+  !>@param[out]  Ainv: real(dl) - nxn array to store the inverse of A
+  !>@param[in]   n : size of arrays
+  subroutine invert_matrix(A, Ainv, n)
+    integer, intent(in) :: n
+    real(dl), dimension(n,n), intent(in) :: A
+    real(dl), dimension(n,n), intent(out) :: Ainv
+
+    ! Workspace for LAPACK
+    real(dl), allocatable, dimension(:) :: work
+    integer :: lwork, lda, info
+    integer, allocatable, dimension(:) :: ipiv
+    integer :: deallocatestatus
+
+  ! check how much space LAPACK needs here
+    lda = n  ! should really assign separately as input
+    lwork = n*n
+    allocate (work(lwork)); allocate (ipiv(n))
+
+! Perform LU decomposition
+    Ainv = A
+    call DGETRF( n, n, Ainv, lda, ipiv, info )
+
+! Add some calls to make sure the LU decomp worked
+    if (info.eq.0) then
+       print*, "LU decomposition successful"
+    elseif (info < 0) then
+       print*, "LU decomposition: illegal value at ", info
+       stop
+    elseif (info > 0) then
+       print*, "Singular Matrix U = 0 at ",info
+    endif
+
+! USE LU decomposition to compute inverse
+! To do: finish workspace query to determine size of work array
+!  call DGETRI(N, Ainv, LDA, IPIV, WORK, -1, INFO)
+!  lwork = work(1)
+!  deallocate(work, 
+!  allocate(work(lwork))
+    call DGETRI( n, Ainv, lda, IPIV, work, lwork, info )
+
+    if (info.ne.0) then
+       stop "Matrix inversion failed!"
+    else
+       print*, "Inverse Successful"
+    endif
+
+    ! clean up temporary workspace
+    deallocate(ipiv, STAT=deallocatestatus)
+    deallocate(work, STAT=deallocatestatus)
+
+  end subroutine invert_matrix
+
+end module Matrix
