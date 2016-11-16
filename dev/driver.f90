a4e881dafdfb36e320849d68ab5b36b41eee37f6
Jonathan Braden <jonathan.n.braden@gmail.com>
2016-11-16 22:14:12 +0000

    Started writing code for scaled field solutions

--- dev/driver.f90
+++ dev/driver.f90
@@ -21,9 +21,15 @@ program Instanton
 
   real(dl) :: delta
 
-  delta = 0.1_dl
-  r0 = 1.5_dl*2._dl**0.5/delta; w0=2.**0.5
-  phif=-1.; phit=1.
+  ! Values of double well
+!  delta = 0.5_dl
+!  r0 = 1.5_dl*2._dl**0.5/delta; w0=2.**0.5
+!  phif=-1.; phit=1.
+
+  delta = 0.55_dl
+  r0 = 3._dl*2._dl**0.5*delta**0.5
+  phif = 0._dl; phit = pi
+
   len = r0*3._dl**0.5
   
   order = 100; n=order+1
@@ -37,7 +43,6 @@ program Instanton
 
   allocate(phi(0:order),phi_prev(0:order))
   call initialise_fields(.false.)
-  
   call get_vacuum(phif); call get_vacuum(phit)
   print*,"vacua are ", phit, phif
   phi = -0.5_dl*(phit-phif)*tanh((transform%xGrid-r0)/w0) + 0.5_dl*(phit+phif)
@@ -148,7 +153,7 @@ contains
 
     sz = size(xvals)
     do i=1,sz
-       write(u,*) xvals(i), phi(i), potential(phi(i)), vdprime(phi(i))
+       write(u,*) xvals(i), phi(i), potential(phi(i)) - potential(phif), vdprime(phi(i))
     enddo
   end subroutine output_simple
 
--- dev/model-canonical.f90
+++ dev/model-canonical.f90
@@ -30,6 +30,23 @@ contains
     del = delta
   end subroutine initialise_equations
 
+  subroutine initialise_equations_scaled(tForm, delta, dim, scl)
+    type(Chebyshev), intent(in) :: tForm
+    real(dl), intent(in) :: delta, scl
+    integer, intent(in) :: dim
+    integer :: i, sz
+    real(dl) :: d, m
+
+    sz = size(tForm%xGrid)
+    allocate( L0(1:sz,1:sz) )
+    d = dble(dim) - 1._dl - 2._dl*scl
+    m = scl*(scl+1._dl-dble(dim)+1._dl)
+    do i=0,sz-1
+       L0(i+1,:) = tForm%derivs(i,:,2) + d*tForm%derivs(i,:,1)/tForm%xGrid(i)
+       L0(i+1,i+1) = L0(i+1,i+1) - m/tForm%xGrid(i)**2
+    enddo
+  end subroutine initialise_equations_scaled
+
 ! Preprocessors for inlining
 !#define POTENTIAL(f) ( 0.25_dl*(f**2-1._dl)**2 + del*(f**3/3._dl-f) )
 !#define VPRIME(f) ( (f+del)*(f**2-1._dl) )
@@ -38,23 +55,22 @@ contains
 #define VPRIME(f) ( -sin(f) + del*sin(2._dl*f) )
 #define VDPRIME(f) ( -cos(f) + 2._dl*del*cos(2._dl*f) )
 
-
   elemental function potential(phi)
     real(dl) :: potential
     real(dl), intent(in) :: phi
-    potential = POTENTIAL(phi) !0.25_dl*(phi**2-1._dl)**2 + del*(phi**3/3._dl - phi)
+    potential = POTENTIAL(phi)
   end function potential
 
   elemental function vprime(phi)
     real(dl) :: vprime
     real(dl), intent(in) :: phi
-    vprime =  VPRIME(phi) !(phi+del)*(phi**2 - 1.)
+    vprime =  VPRIME(phi)
   end function vprime
 
   elemental function vdprime(phi)
     real(dl) :: vdprime
     real(dl), intent(in) :: phi
-    vdprime =  VDPRIME(phi) !3.*phi**2 - 1. + 2.*del*phi
+    vdprime =  VDPRIME(phi)
   end function vdprime
   
   subroutine source(fld,src)
@@ -68,6 +84,21 @@ contains
     src(sz) = 0._dl  ! Set boundary condition at infinity
   end subroutine source
 
+  subroutine source_scaled(fld,src,scl,rvals)
+    real(dl), dimension(1:), intent(in) :: fld
+    real(dl), dimension(1:), intent(out) :: src
+    real(dl), intent(in) :: scl
+    real(dl), dimension(1:), intent(in) :: rvals
+    real(dl), dimension(:), allocatable :: ftmp
+    integer :: sz
+    
+    sz = size(fld); allocate( ftmp(sz) )
+    ftmp = fld / rvals**scl
+    src(:) = -matmul(L0,fld)
+    src(:) = src(:) + rvals**scl*VPRIME(ftmp)
+    deallocate(ftmp)
+  end subroutine source_scaled
+
   subroutine variation(fld,var)
     real(dl), dimension(1:), intent(in) :: fld
     real(dl), dimension(1:,1:), intent(out) :: var
@@ -76,12 +107,32 @@ contains
     sz = size(fld)
     var(1:sz,1:sz) = L0(1:sz,1:sz)
     do i=1,sz
-       var(i,i) = var(i,i) - VDPRIME(fld(i)) !- (3._dl*fld(i)**2-1._dl +2._dl*del*fld(i))
+       var(i,i) = var(i,i) - ( VDPRIME(fld(i)) ) !- (3._dl*fld(i)**2-1._dl +2._dl*del*fld(i))
     enddo
     ! boundary condition at infinity
     var(sz,:) = 0._dl
     var(sz,sz) = 1._dl
   end subroutine variation
+
+  subroutine variation_scaled(fld,var,scl,rvals)
+    real(dl), dimension(1:), intent(in) :: fld
+    real(dl), dimension(1:,1:), intent(out) :: var
+    real(dl), intent(in) :: scl
+    real(dl), dimension(1:), intent(in) :: rvals
+    integer :: i, sz
+    real(dl), dimension(:), allocatable :: ftmp
+
+    sz = size(fld); allocate( ftmp(sz) )
+    ftmp = fld / rvals**scl
+
+    var(1:sz,1:sz) = L0(1:sz,1:sz)
+    do i=1,sz
+       var(i,i) = var(i,i) - rvals(i)**scl*( VDPRIME(ftmp(i)) ) !- (3._dl*fld(i)**2-1._dl +2._dl*del*fld(i))
+    enddo
+    ! boundary condition at infinity
+    var(sz,:) = 0._dl
+    var(sz,sz) = 1._dl
+  end subroutine variation_scaled
 
   !>@brief
   !> A subroutine to set general Robin boundary conditions on our fields
