c04cec56f274ad1cb0204e918569d7a7833915e8
Jonathan Braden <jonathan.n.braden@gmail.com>
2016-11-16 18:28:43 +0000

    Inlining of potential functions in model-canonical.  Included Drummond potentials

--- dev/model-canonical.f90
+++ dev/model-canonical.f90
@@ -28,32 +28,35 @@ contains
        L0(i+1,:) = tForm%derivs(i,:,2) + 3._dl*tForm%derivs(i,:,1)/tForm%xGrid(i)
     enddo
     del = delta
-    ! In the original code, there's a multiplication by the MMT.  Make sure that this is already included in my definition of derivs
   end subroutine initialise_equations
 
-!
-! Define the model through it's potential
-!
+! Preprocessors for inlining
+!#define POTENTIAL(f) ( 0.25_dl*(f**2-1._dl)**2 + del*(f**3/3._dl-f) )
+!#define VPRIME(f) ( (f+del)*(f**2-1._dl) )
+!#define VDPRIME(f) ( 3._dl*f**2 - 1._dl + 2._dl*del*f )
+#define POTENTIAL(f) ( cos(f) + del*sin(f)**2 + 1._dl )
+#define VPRIME(f) ( -sin(f) + del*sin(2._dl*f) )
+#define VDPRIME(f) ( -cos(f) + 2._dl*del*cos(2._dl*f) )
+
+
   elemental function potential(phi)
     real(dl) :: potential
     real(dl), intent(in) :: phi
-    potential = 0.25_dl*(phi**2-1._dl)**2 + del*(phi**3/3._dl - phi)
+    potential = POTENTIAL(phi) !0.25_dl*(phi**2-1._dl)**2 + del*(phi**3/3._dl - phi)
   end function potential
 
   elemental function vprime(phi)
     real(dl) :: vprime
     real(dl), intent(in) :: phi
-    vprime =  (phi+del)*(phi**2 - 1.)
+    vprime =  VPRIME(phi) !(phi+del)*(phi**2 - 1.)
   end function vprime
 
   elemental function vdprime(phi)
     real(dl) :: vdprime
     real(dl), intent(in) :: phi
-    vdprime =  3.*phi**2 - 1. + 2.*del*phi
+    vdprime =  VDPRIME(phi) !3.*phi**2 - 1. + 2.*del*phi
   end function vdprime
   
-! Preprocessor for inlining
-#define VPRIME(f) ((f+del)*(f**2-1._dl))
   subroutine source(fld,src)
     real(dl), dimension(1:), intent(in) :: fld
     real(dl), dimension(1:), intent(out) :: src
@@ -61,11 +64,10 @@ contains
 
     sz = size(fld)
     src(:) = -matmul(L0,fld)
-    src(:) = src(:) + (fld(:)+del)*(fld(:)**2-1._dl)  !VPRIME(fld(:))
+    src(:) = src(:) + VPRIME(fld(:)) !+ (fld(:)+del)*(fld(:)**2-1._dl)
     src(sz) = 0._dl  ! Set boundary condition at infinity
   end subroutine source
 
-#define VDPRIME(f) (3._dl*f**2 - 1._dl + 2._dl*del*f)
   subroutine variation(fld,var)
     real(dl), dimension(1:), intent(in) :: fld
     real(dl), dimension(1:,1:), intent(out) :: var
@@ -74,7 +76,7 @@ contains
     sz = size(fld)
     var(1:sz,1:sz) = L0(1:sz,1:sz)
     do i=1,sz
-       var(i,i) = var(i,i) - (3._dl*fld(i)**2-1._dl +2._dl*del*fld(i))  !VDPRIME(fld(i))
+       var(i,i) = var(i,i) - VDPRIME(fld(i)) !- (3._dl*fld(i)**2-1._dl +2._dl*del*fld(i))
     enddo
     ! boundary condition at infinity
     var(sz,:) = 0._dl
