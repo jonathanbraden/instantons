7408424d283f840c4ef8082f1604c0487bbd019f
Jonathan Braden <jonathan.n.braden@gmail.com>
2016-11-29 16:28:32 +0000

    More debugging in solver

--- dev/solver.f90
+++ dev/solver.f90
@@ -10,24 +10,37 @@
 !>
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-#define SOLVER_DEBUG 1
+#define DEBUG_SOLVER 1
 
 module Nonlinear_Solver
   use constants
   use Cheby
   use Model
   implicit none
+
+  real(dl), external :: DLANGE
+  
+  type Solver_Storage
+     integer, dimension(:), allocatable :: iwork
+     real(dl), dimension(:), allocatable :: rwork
+     real(dl), dimension(:,:), allocatable :: lu_factor
+     real(dl), dimension(:), allocatable :: row_scale, col_scale
+  end type Solver_Storage
   
   type Solver
      integer :: nVar, u
+     real(dl) :: alpha
      integer :: maxIter; real(dl) :: kick_param  ! Adjustable solver parameters
      real(dl), dimension(:,:), allocatable :: L, L_const  ! Linear operator
      real(dl), dimension(:), allocatable :: S    ! Source term
      real(dl), dimension(:), allocatable :: del  ! Residual and perturbation
      ! Make these ones private
      integer, dimension(:), allocatable :: ipiv  ! Needed for DGESV in the solver
-     real(dl), dimension(:), allocatable :: f_prev
+     real(dl), dimension(:), allocatable :: f_prev, S_prev
      logical :: spec_space  ! whether or not we work in spectral space (not yet used)
+#ifdef DEBUG_SOLVER
+     type(Solver_Storage) :: mat_store
+#endif
   end type Solver
 
 !  abstract interface
@@ -39,6 +52,14 @@ module Nonlinear_Solver
 
 contains
 
+  !>@brief
+  !> Set aside storage space and necessary variables for our nonlinear iterations used
+  !> to solve a boundary value problem
+  !>
+  !>@param[in,out] this
+  !>@param[in] n  Number of variables to solve for
+  !>@param[in] nit Maximum number of iterations for the solver before quitting
+  !>@param[in] kick Parameter controlling the ability to leave a local minimum (currently not functional)
   subroutine create_solver(this,n, nit, kick)
     type(Solver), intent(inout) :: this
     integer, intent(in) :: n,nit
@@ -47,10 +68,22 @@ contains
     allocate(this%L(1:n,1:n),this%L_const(1:n,1:n)); allocate(this%S(1:n))
     allocate(this%del(1:n)); allocate(this%f_prev(1:n))
     allocate(this%ipiv(1:n))
-    
+    allocate(this%S_prev(1:n))
+#ifdef DEBUG_SOLVER
+    call create_solver_storage(this%mat_store,n)
+#endif
+
     this%u = 99  ! Change this to a call to the next open solver
     open(unit=this%u,file='solver-output.dat')
   end subroutine create_solver
+
+  subroutine create_solver_storage(this, n)
+    type(Solver_Storage), intent(out) :: this
+    integer, intent(in) :: n
+
+    allocate(this%iwork(1:n)); allocate(this%rwork(1:4*n))
+    allocate(this%lu_factor(1:n,1:n)); allocate(this%row_scale(1:n),this%col_scale(1:n))
+  end subroutine create_solver_storage
 
   subroutine delete_solver(this)
     type(Solver), intent(inout) :: this
@@ -70,6 +103,8 @@ contains
        call output_solver(this)
        if (stop_solver(this)) then; print*,"finished in ",i," iterations"; exit; endif
     enddo
+    print*,""
+    write(this%u,*) ""
   end subroutine solve
   
   !>@brief
@@ -106,15 +141,35 @@ contains
     real(dl) :: alpha
     real(dl) :: err_rms, err_max, res
     real(dl) :: b1  ! remove this later
-    
+#ifdef DEBUG_SOLVER
+    real(dl) :: cond_num, mat_norm(1:2)
+    character :: norm
+#endif
+
     n = this%nVar
     
     call variation(f_cur, this%L)
     call source(f_cur, this%S)
     
     res = sqrt(sum(this%S**2))  ! Current residual
+    this%S_prev = this%S
     ! Compute the required perturbation
     call DGESV(n,1,this%L,n,this%ipiv,this%S,n,info)
+#ifdef DEBUG_SOLVER
+    !mat_norm = F06RAF('1',n,n,this%L,n, ) ! NAG version of DLANGE
+    mat_norm(1) = DLANGE('1',n,n,this%L,n,this%mat_store%rwork(1:n))
+    mat_norm(2) = DLANGE('I',n,n,this%L,n,this%mat_store%rwork(1:n))
+    call DGECON('1',n,this%L,n,mat_norm,cond_num,this%mat_store%rwork(1:4*n),this%mat_store%iwork(1:n),info)
+    print*,"One-norm condition number of matrix is ", cond_num
+    call DGECON('I',n,this%L,n,mat_norm,cond_num,this%mat_store%rwork(1:4*n),this%mat_store%iwork(1:n),info)
+    print*,"Infinity-norm condition number of matrix is ", cond_num
+    call DGETRF(n,n,this%L,n,this%ipiv,info) ! Get LU factorisation
+    call DGETRS('N',n,1,this%L,n,this%ipiv,this%S,n,info)
+    ! Do eigenvalue decomposition
+
+!    call DGESVX(,'N',n,1,this%L,n,this%lu_factor,n,this%ipiv,'N', , ,this%S,n, ,
+#endif
+
     this%del = this%S
     if (info /= 0) then
        print*,"Error inverting linear matrix in solver"
@@ -132,14 +187,18 @@ contains
        call source(this%f_prev, this%S) ! replace with a call to source
        err_max = maxval(abs(this%S))
        err_rms = sqrt(sum(this%S**2))
-!       print*,"b1 is ",err_rms**2," b0 is ",res**2," alpha is ",alpha
+#ifdef DEBUG_SOLVER
+       print*,"RMS error is ",err_rms**2," Residual is ",res**2," alpha is ",alpha
+#endif
        if (err_rms < res) exit
     enddo
-
-!    print*,""
+#ifdef DEBUG_SOLVER
+    print*,""
+#endif
     this%f_prev = f_cur            ! Store previous iteration
     f_cur = f_cur + alpha*this%del  ! Update function
-    call source(f_cur, this%S)          ! Store violation of the equation of motion
+    call source(f_cur, this%S)
+    this%alpha = alpha          ! Store violation of the equation of motion
   end subroutine line_iteration
 
   !>@brief
@@ -193,7 +252,7 @@ contains
     integer :: i
 
     do i=1,this%nVar
-       write(this%u,*) this%f_prev(i), this%del(i), this%S(i)
+       write(this%u,*) this%f_prev(i), this%del(i), this%S(i), this%S_prev(i)
     enddo
     write(this%u,*)
   end subroutine output_solver
@@ -204,6 +263,7 @@ contains
     type(Solver), intent(in) :: this
 
     print*,"RMS violation of the EOM is ",sqrt(sum(this%S**2))
+    print*,"Alpha on previous step is ",this%alpha
   end subroutine print_solver
 
 end module Nonlinear_Solver
