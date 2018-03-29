0cac1d68198845a74afe3f986295de2b05937247
Jonathan Braden <jonathan.n.braden@gmail.com>
2018-03-29 17:28:57 +0100

    Code cleanup and better locality in driver

--- dev/driver.f90
+++ dev/driver.f90
@@ -7,33 +7,27 @@ program Instanton
 
 ! Nonlinear Solver Parameters and storage.  To be moved to a separate module upon code cleanup
 !  type(Field_Model) :: model
-
-  real(dl), dimension(:), allocatable :: xNew
-  
+ 
   type Solution
      type(Chebyshev) :: tForm
      real(dl), dimension(:), allocatable :: phi
   end type Solution
   
-  type(Solver) :: solv
-  real(dl), dimension(:), allocatable :: phi, phi_prev
+  real(dl), dimension(:), allocatable :: phi
   real(dl), dimension(:), allocatable :: model_params
 
-  integer :: order, n
-  real(dl) :: w, len  ! parameters controlling mapping of collocation grid
-  real(dl), parameter :: wbase = 10._dl !20._dl
-  real(dl) :: meff
+  integer :: order_, n
   integer :: i
 
   ! Move these parameters somewhere else
 !  type(Bubble_Params) :: bubble
-  real(dl) :: phit, phif,r0,w0
-  real(dl) :: w_old, l_old
+
+  real(dl) :: phit, phif  ! kill these to restore locality, currently used in output subroutine
   
   real(dl) :: delta
   real(dl), allocatable :: deltas(:); integer :: nDel
   integer :: dim 
-
+  
   dim = 1
   nDel = 50; allocate(deltas(1:nDel))
   deltas = 0.5_dl + 10.**([ (-6.+0.2*(i-1), i=size(deltas),1,-1) ])
@@ -43,54 +37,13 @@ program Instanton
 !  r0 = 1.5_dl*2._dl**0.5/delta; w0=2.**0.5
 !  phif=-1.; phit=1.
 
-!  delta = 0.54_dl 0.56_dl
-  delta = deltas(1) 
+  order_=100
+  allocate(phi(0:order_))
+  call compute_profile(0.5*1.3_dl**2,order_,dim,phi,out=.true.)
   
-  phif = 0._dl; phit = pi
-  order = 200; n=order+1
-  allocate(phi(0:order),phi_prev(0:order))  ! This is ugly, fix it to allow for varying orders
-
-!  call bubble_parameters(delta,r0,meff);  call grid_params(w,len,r0,1._dl/meff)
-  call bubble_parameters_nd(delta,dim*1._dl,r0,meff); call grid_params(w,len,r0,1._dl/meff)
-  print*,"params are ",r0,meff
-  call create_grid(transform,order,w,len)
-  print*, transform%xGrid(:10)
-  
-  open(unit=80,file='actions.dat')
-  call compute_profile(delta,order,phi)
-  write(80,*) delta, eval_action(phi,transform,dim,r0)
-  
-  allocate(xNew(0:order)) ! Remove this if necessary
-  open(unit=80,file='actions.dat')
-  do i=2,size(deltas)
-     delta = deltas(i); print*,"delta = ",delta
-!     call bubble_parameters(delta,r0,meff);  call grid_params(w,len,r0,1._dl/meff)
-     call bubble_parameters_nd(delta,dim*1._dl,r0,meff); call grid_params(w,len,r0,1._dl/meff)
-     if (phi(0) < 0.9*phit) then
-        print*,"Using old solution"
-        xNew = chebyshev_grid(order,len,w)
-        phi_prev = interpolate_instanton(xNew,phi,transform)
-        call compute_profile(delta,order,phi,phi_prev)
-     else
-        call compute_profile(delta,order,phi)
-     endif
-     write(80,*) delta, eval_action(phi,transform,dim,r0) 
-  enddo
-  close(80)
-  
-!  call scan_profiles(deltas)
+  call scan_profiles(deltas,dim,200)
   
 contains
-
-  subroutine interpolate_new_field(order,phi_old,phi_new,tForm)
-    integer, intent(in) :: order
-    real(dl), dimension(0:order), intent(in) :: phi_old
-    real(dl), dimension(0:order), intent(out) :: phi_new
-    type(Chebyshev), intent(in) :: tForm
-    real(dl), dimension(0:order) :: xNew
-    xNew = chebyshev_grid(order,len,w)
-    
-  end subroutine interpolate_new_field
   
   ! GRRR, this thing is all fucked because of the order I need to compute things in.  Design a useful way to modularise it
   
@@ -133,7 +86,6 @@ contains
        endif
     endif
   end subroutine initialise_fields
-
   
   subroutine initial_profile(phi,delta,phi_prev)
     type(Solution), intent(out) :: phi, phi_prev
@@ -147,88 +99,86 @@ contains
     ! Add interpolation and condition above
     else
        call breather_profile(phi%tForm%xGrid,phi%phi,r0,1./meff)  ! ugly nonlocality with transform
-    endif
-       
+    endif       
   end subroutine initial_profile
-  
-  subroutine scan_profiles(delVals)
+
+!!!! TO FIX: nonlocality of w,r0,len,meff in this subroutine
+  !!!! Extra calls to create chebyshev grids
+  subroutine scan_profiles(delVals,dim,ord)
     real(dl), dimension(:), intent(in) :: delVals
+    integer, intent(in) :: dim, ord
+
+    real(dl) :: delta
+    real(dl) :: w, len, r0, meff
+    real(dl) :: phit, phif
+    real(dl), dimension(0:ord) :: xNew, phi_prev, phi
+    integer, parameter :: actFile=80
+
+    phit = pi; phif = 0._dl  ! replace with function call
     
-    open(unit=80,file='integrals.dat')
-    do i=1,size(delVals)
+    open(unit=actFile,file='actions.dat')
+
+    delta = delVals(1)
+    call compute_profile(delta,ord,dim,phi,out=.true.)
+    write(actFile,*) delta, eval_action(phi,transform,dim,r0)
+    
+    do i=2,size(delVals)
        delta = delVals(i)
-       r0 = 3._dl*2._dl**0.5*delta**0.5; w0=2.**0.5;
-       meff = (2._dl*delta)**0.5*(1._dl-0.25_dl/delta**2)**0.5
-       len=r0*3._dl**0.5; w = wbase/len/meff
-       call destroy_chebyshev(transform)
-       call create_grid(transform,order,w,len)
-       call initialise_equations(transform,delta,3)
-       !     if (meff*r0 < 2._dl) then
-       if (.false.) then
-          print*,"using prev, for delta = ", delta
-          phi = interpolate_instanton(transform%xGrid,phi_prev,transform) ! Need to store old transform
+       if (phi(0) < 0.9*phit) then
+          call bubble_parameters_nd(delta,dim*1._dl,r0,meff); call grid_params(w,len,r0,1._dl/meff)
+          xNew = chebyshev_grid(ord,len,w)
+          phi_prev = interpolate_instanton(xNew,phi,transform)  ! horrible nonlocality with transform
+          call compute_profile(delta,ord,dim,phi,phi_prev)
        else
-          call initialise_fields(phi,delta,.false.)
+          call compute_profile(delta,ord,dim,phi)
        endif
-       call solve(solv, phi)
-       !call print_solver(solv)
-       !call output_simple(transform,phi,.false.)
-       print*,delta, eval_action(phi,transform,3,r0)
-       write(80,*) delta, eval_action(phi,transform,3,r0)
-       w_old = w; l_old = len; phi_prev = phi
+       write(actFile,*) delta, eval_action(phi,transform,dim,r0)
     enddo
+    close(actFile)
   end subroutine scan_profiles
-  
-  !>@todo
-  !>@arg Write this.  It will have identical functionality to the compute_profile subroutine, just with a function return.  For easiest maintainability, probably easiest to just call the subroutine
-  function instanton_profile(delta,order,phi_init) result(phi)
-    real(dl), intent(in) :: delta
-    integer, intent(in) :: order
-    real(dl), dimension(0:order), optional :: phi_init
-    real(dl), dimension(0:order) :: phi
-  end function instanton_profile
   
   !>@brief
   !> Solve for a single bubble profile.  Optionally, store the output field values, derivatives, potential, etc. in a file.
-  subroutine compute_profile(delta, order, phi, phi_init)
+  subroutine compute_profile(delta, order, dim, phi, phi_init, out)
     real(dl), intent(in) :: delta
-    integer, intent(in) :: order
-    real(dl), dimension(0:order) :: phi
+    integer, intent(in) :: order, dim
+    real(dl), dimension(0:order), intent(out) :: phi
     real(dl), intent(in), optional :: phi_init(0:order)
-    
+    logical, intent(in), optional :: out
+
+!    type(Chebyshev) :: tForm
+    logical :: outLoc
     type(Solver) :: solv
     real(dl) :: w,len         ! Collocation grid parameters
-    real(dl) :: r0, w0, meff  ! Bubble Parameters
+    real(dl) :: r0, meff  ! Bubble Parameters
     real(dl) :: phif, phit    ! Move this elsewhere
     integer :: n
-!    integer, parameter :: dim = 3
-    
+
+    outLoc = .false.; if (present(out)) outLoc = out
     n = order+1
 
 !   call get_minima(phif,phit)
     phif = 0._dl; phit = pi  ! Replace this with the function call above
 
-!    call bubble_parameters(delta,r0,meff)
     call bubble_parameters_nd(delta,dim*1._dl,r0,meff)
     call grid_params(w,len,r0,1._dl/meff)
 
-    print*,"In compute profile r0=",r0,"meff = ",meff
     ! The way transform is used here is ugly and nonlocal.  Fix it!!!
     call destroy_chebyshev(transform)
     call create_grid(transform,order,w,len)
     call create_solver(solv,n,100,0.1_dl)
     call initialise_equations(transform,delta,dim)
-
+    
     ! Modularise this part
     if (present(phi_init)) then
        phi(0:order) = phi_init(0:order)
     else
-       !!! Need to fix this now, since I have a different meff in the subroutine
+!!! Need to fix this now, since I have a different meff in the subroutine
        call initialise_fields(phi,delta,.false.)  ! Replace this with a call to thin-wall profile or something
     endif
     call solve(solv,phi)
-    print*,eval_action(phi,transform,dim,r0)
-    call output_simple(transform,phi,.false.)
+
+    if (outLoc) call output_simple(transform,phi,.false.)
   end subroutine compute_profile
 
   !>@brief
@@ -270,16 +220,7 @@ contains
     meff = (2._dl*delta)**0.5
     r0 = dim*(2._dl*delta)**0.5
   end subroutine bubble_parameters_nd
-  
-  subroutine bubble_parameters(delta,r0,meff)
-    real(dl), intent(in) :: delta
-    real(dl), intent(out) :: r0, meff
-
-    meff = (2._dl*delta)**0.5!*(1._dl-0.25/delta**2)**0.5
-    r0 = 3._dl*(2._dl*delta)**0.5
-!    w0 = 1._dl/(2._dl*delta)**0.5  ! Adjust as necessary
-  end subroutine bubble_parameters
-  
+    
   subroutine grid_params(w,len,r0,w0)
     real(dl), intent(out) :: w, len
     real(dl), intent(in) :: r0,w0
@@ -287,7 +228,6 @@ contains
     len = r0*3._dl**0.5
     w = wscl * w0 / len 
   end subroutine grid_params
-
     
   subroutine bubble_params(delta,r0,w0,len,w)
     real(dl), intent(in) :: delta
@@ -312,16 +252,19 @@ contains
     call transform_double_infinite(tForm,l)
   end subroutine create_grid
 
-  subroutine interpolate_phi(phi_new, xNew, phi_old, xOld)
+  !! This is super wasteful if we are going to interpolate onto the same grid many times
+  !! If we are, should store the interpolation matrix separately and just do matrix multiplies
+  subroutine interpolate_phi(phi_new, xNew, phi_old, xOld, tForm)
     real(dl), dimension(1:), intent(out) :: phi_new
     real(dl), dimension(1:), intent(in) :: xNew, phi_old, xOld
+    type(Chebyshev), intent(in) :: tForm
     real(dl), dimension(:), allocatable :: phi_spec
     real(dl), dimension(:,:), allocatable :: basis
     integer :: i, ord
 
     ord = size(phi_old)-1
     allocate( phi_spec(0:ord) ); allocate(basis(0:ord,0:2))
-    phi_spec = matmul(transform%fTrans, phi_old)
+    phi_spec = matmul(tForm%fTrans, phi_old)
     do i=1,size(xNew)
        call evaluate_chebyshev(ord,xNew(i),basis,0)
        phi_new(i) = sum(phi_spec(:)*basis(:,0))
@@ -357,7 +300,7 @@ contains
        endif
     endif
 
-    call variation(phi,L)  ! Change this if different boundary conditions are needed.  Currently is will barf since it's getting the wrong boundaries for the eigenvalue problem
+    call variation(phi,L)  ! Change this if different boundary conditions are needed.  Currently this will barf since it's getting the wrong boundaries for the eigenvalue problem
 
     call DGEEV('N','N',n,L,n,eval_real,eval_imag,dummy,1,dummy,1,work,iwork,ierror)
     if (ierror /= 0) then
@@ -411,7 +354,8 @@ contains
     integer, parameter :: u = 60  ! Fix this ugliness
     real(dl), dimension(1:size(phi)) :: phi_spec, dphi, d2phi
     logical :: o
-    
+
+    print*,"Outputting profile"
     inquire(opened=o,unit=u)
     if (.not.o) open(unit=u,file='instanton.dat')
 
@@ -450,60 +394,24 @@ contains
     enddo
     write(u,*)
   end subroutine output
-    
-  !>@brief
-  !> Compute the instanton action given the transform grid and field values
-  subroutine get_action(fld,transform)
-    real(dl), dimension(:), intent(in) :: fld
-    type(Chebyshev), intent(in) :: transform
-    real(dl), dimension(1:size(fld)) :: dfld, lag
-    real(dl) :: action, tension, KE, PE
-    
-#ifdef USE_BLAS
-    call DGEMV()
-#else
-    dfld = matmul(transform%derivs(:,:,1),fld)
-#endif
-    lag = 0.5_dl*dfld**2 + potential(fld) - potential(phif)
-    action = quadrature(transform,lag*transform%xGrid(:)**3)
-    tension = quadrature(transform,dfld**2)
-    KE = quadrature(transform,0.5_dl*dfld**2*transform%xGrid(:)**3)
-    PE = quadrature(transform,(potential(fld)-potential(phif))*transform%xGrid(:)**3)
-    
-    print*,"integrals are",action,tension,KE,PE,0.5_dl*tension*r0**3
-    write(80,*) delta, action, tension, KE, PE, tension*r0**3
-  end subroutine get_action
-
-#define V_TW(f) ( del*sin(f)**2 )  ! This isn't going to work anymore since del is private
-  function new_action(sol,d,r0) result(action)
-    real(dl), dimension(1:7) :: action
-    type(Solution), intent(in) :: sol
-    integer, intent(in) :: d
-    real(dl), intent(in) :: r0
-    real(dl), dimension(size(sol%phi)) :: dfld, lag
-
-    dfld = matmul(sol%tForm%derivs(:,:,1),sol%phi)
-    lag = 0.5_dl*dfld**2 + potential(sol%phi) - potential(phif)
-    action = 0._dl
-  end function new_action
-   
-  function eval_action(fld,transform,d,r0) result(action)
+       
+  function eval_action(fld,tForm,d,r0) result(action)
     real(dl), dimension(1:7) :: action
     real(dl), dimension(:), intent(in) :: fld
-    type(Chebyshev), intent(in) :: transform
+    type(Chebyshev), intent(in) :: tForm
     integer, intent(in) :: d
     real(dl), intent(in) :: r0
     real(dl), dimension(1:size(fld)) :: dfld, lag
 
-    dfld = matmul(transform%derivs(:,:,1),fld)
+    dfld = matmul(tForm%derivs(:,:,1),fld)
     lag = 0.5_dl*dfld**2 + potential(fld) - potential(phif)
-    action(1) = quadrature(transform,lag*transform%xGrid(:)**d)
+    action(1) = quadrature(transform,lag*tForm%xGrid(:)**d)
     action(2) = quadrature(transform,dfld**2)
-    action(3) = quadrature(transform,0.5_dl*dfld**2*transform%xGrid(:)**d)
-    action(4) = quadrature(transform,(potential(fld)-potential(phif))*transform%xGrid(:)**d)
+    action(3) = quadrature(transform,0.5_dl*dfld**2*tForm%xGrid(:)**d)
+    action(4) = quadrature(transform,(potential(fld)-potential(phif))*tForm%xGrid(:)**d)
     action(5) = 0.5_dl*action(2)*r0**d
-    action(6) = quadrature(transform,(0.5_dl*dfld**2+potential_tw(fld))*transform%xGrid(:)**d)
-    action(7) = quadrature(transform,fld*vprime(fld)*transform%xGrid(:)**d)
+    action(6) = quadrature(transform,(0.5_dl*dfld**2+potential_tw(fld))*tForm%xGrid(:)**d)
+    action(7) = quadrature(transform,fld*vprime(fld)*tForm%xGrid(:)**d)
     ! Add in the "thin-wall potential" here
   end function eval_action
 
@@ -511,8 +419,9 @@ contains
   function interpolate_instanton(r_new,f_cur,tForm) result(f_int)
     real(dl), dimension(:), intent(in) :: r_new, f_cur
     type(Chebyshev), intent(in) :: tForm
-    real(dl) :: L,w
     real(dl), dimension(1:size(r_new)) :: f_int
+
+    real(dl) :: L,w
     real(dl) :: xvals(1:size(r_new)), spec(1:size(f_cur)),  bVals(1:size(f_cur),0:2)
     integer :: i; real(dl) :: winv
 
@@ -521,7 +430,6 @@ contains
     xvals = r_new / sqrt(r_new**2+L**2)
     xvals = atan(winv*tan(pi*(xvals-0.5_dl)))/pi + 0.5_dl
     xvals = 2._dl*xvals**2-1._dl
-
 #ifdef USEBLAS
 
 #else
@@ -562,7 +470,7 @@ contains
 
   !>@brief
   !> Return Gauss-Lobatto Grid for the even Chebyshevs
-  function chebyshev_grid_lobattor(order,L,w) result(xVals)
+  function chebyshev_grid_lobatto(order,L,w) result(xVals)
     integer, intent(in) :: order
     real(dl), intent(in) :: L,w
     real(dl), dimension(0:order) :: xVals
@@ -575,6 +483,6 @@ contains
     xVals = sqrt(0.5_dl*xVals + 0.5_dl)
     xVals = (1._dl/pi)*atan(w*tan(pi*(xVals-0.5_dl))) + 0.5_dl
     xVals = L*xVals / sqrt(1._dl - xVals**2)
-  end function chebyshev_grid_lobattor
+  end function chebyshev_grid_lobatto
   
 end program instanton
