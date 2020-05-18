65fcfc8c5990573852a44b4b884eecad5a122f1b
Jonathan Braden <jonathan.n.braden@gmail.com>
2020-05-18 12:25:25 -0400

    Fixed incorrect derivatives in log potential

--- dev/model-log-pot.f90
+++ dev/model-log-pot.f90
@@ -10,22 +10,36 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 ! Preprocessors for inlining.  Could also move this to another file
-#define POTENTIAL(f) ( 0.5_dl*f**2*(1._dl-log(f**2+del**2)) )
-#define VPRIME(f) ( -f*(log(f**2+del**2)-del**2) )
-#define VDPRIME(f) ( -log(f**2+del**2) - 2._dl*f**2/(f**2+del**2) + del**2 )
+!#define POTENTIAL(f) ( 0.5_dl*(eps**2+1._dl)*f**2 - 0.5_dl*(f**2+eps**2)*log(f**2 + eps**2) )
+!#define VPRIME(f) ( -f*(log(f**2+eps**2) - eps**2) )
+!#define VDPRIME(f) ( -log(f**2+eps**2) - 2._dl*f**2/(f**2+eps**2) + eps**2 )
+
+#define POTENTIAL(f) ( 0.5_dl*f**2*(1._dl-log(f**2 + eps**2)) )
+#define VPRIME(f) ( -f*(log(f**2+eps**2) - eps**2/(f**2+eps**2)) )
+#define VDPRIME(f) ( -log(f**2+eps**2) + (eps**4-3._dl*eps**2*f**2-2._dl*f**4)/(eps**2+f**2)**2 )
+
+! This set isn't working yet
+!#define POTENTIAL(f) ( 0.5_dl*(f**2-(f**2+eps**2)*log(f**2 + eps**2) + eps**2*log(eps**2)) )
+!#define VPRIME(f) ( -f*log(f**2+eps**2) )
+!#define VDPRIME(f) ( -log(f**2+eps**2) - 2._dl*f**2/(f**2+eps**2) )
+
 
 module Model
   use constants
   use Cheby
   implicit none
-  private :: L0, ndim, del
+  private :: eps
   
-  real(dl), dimension(:,:), allocatable :: L0
-  real(dl) :: del
-  integer :: ndim
+  real(dl) :: eps
 
 contains
 
+  subroutine set_model_params(params,dim)
+    real(dl), dimension(1), intent(in) :: params
+    integer, intent(in) :: dim
+    eps = params(1)
+  end subroutine set_model_params
+  
   subroutine grid_params_(w,len,r0,w0)
     real(dl), intent(out) :: w, len
     real(dl), intent(in) :: r0, w0
@@ -46,39 +60,6 @@ contains
     phif = 0._dl; phit = 10._dl
   end subroutine get_minima
 
-  subroutine initialise_equations(tForm, delta, dim)
-    type(Chebyshev), intent(in) :: tForm
-    real(dl), intent(in) :: delta
-    integer, intent(in) :: dim
-    integer :: i, sz
-    
-    sz = size(tForm%xGrid); ndim = dim
-    if (allocated(L0)) deallocate(L0); allocate( L0(1:sz,1:sz) )
-    do i=0,sz-1
-       L0(i+1,:) = tForm%derivs(i,:,2) + dble(dim)*tForm%derivs(i,:,1)/tForm%xGrid(i)
-    enddo
-    del = delta
-  end subroutine initialise_equations
-
-  subroutine initialise_equations_scaled(tForm, delta, dim, scl)
-    type(Chebyshev), intent(in) :: tForm
-    real(dl), intent(in) :: delta, scl
-    integer, intent(in) :: dim
-    integer :: i, sz
-    real(dl) :: d, m
-
-    sz = size(tForm%xGrid)
-    if (allocated(L0)) deallocate(L0)
-    allocate( L0(1:sz,1:sz) )
-    d = dble(dim) - 2._dl*scl
-    m = scl*(scl+1._dl-dble(dim))
-    do i=0,sz-1
-       L0(i+1,:) = tForm%derivs(i,:,2) + d*tForm%derivs(i,:,1)/tForm%xGrid(i)
-       L0(i+1,i+1) = L0(i+1,i+1) + m/tForm%xGrid(i)**2
-    enddo
-  end subroutine initialise_equations_scaled
-
-  ! Move these to another module
   elemental function potential(phi)
     real(dl) :: potential
     real(dl), intent(in) :: phi
@@ -97,82 +78,13 @@ contains
     vdprime =  VDPRIME(phi)
   end function vdprime
 
-  !!!!!!!!!!!!!!!!!!! This isn't well-defined for the Logarithmic potential
   !>@brief
   !> The unperturbed part of the potential used in computing the thin-wall domain wall
   elemental function potential_tw(phi)
     real(dl) :: potential_tw
     real(dl), intent(in) :: phi
-    potential_tw = del*sin(phi)**2
+    potential_tw = 0._dl
   end function potential_tw
-  
-  subroutine source(fld,src)
-    real(dl), dimension(1:), intent(in) :: fld
-    real(dl), dimension(1:), intent(out) :: src
-    integer :: sz
-
-    sz = size(fld)
-    src(:) = -matmul(L0,fld)
-    src(:) = src(:) + VPRIME(fld(:))
-    src(sz) = 0._dl  ! Set boundary condition at infinity
-  end subroutine source
-
-  !>@brief
-  !> The second variation of our nonlinear equation
-  !
-  !> Notes on storage.  The first index is ? and the second index is ?
-  !> The boundary conditions are designed to pick out phi(r_max),
-  !> which is then set to 0 with the source term
-  subroutine variation(fld,var)
-    real(dl), dimension(1:), intent(in) :: fld
-    real(dl), dimension(1:size(fld),1:size(fld)), intent(out) :: var
-    integer :: i, sz
-
-    sz = size(fld)
-    var(1:sz,1:sz) = L0(1:sz,1:sz)
-    do i=1,sz
-       var(i,i) = var(i,i) - ( VDPRIME(fld(i)) ) 
-    enddo
-    ! boundary condition at infinity
-    var(sz,:) = 0._dl
-    var(sz,sz) = 1._dl
-  end subroutine variation
-
-  subroutine source_scaled(fld,src,scl,rvals)
-    real(dl), dimension(1:), intent(in) :: fld
-    real(dl), dimension(1:), intent(out) :: src
-    real(dl), intent(in) :: scl
-    real(dl), dimension(1:), intent(in) :: rvals
-    real(dl), dimension(:), allocatable :: ftmp
-    integer :: sz
-    
-    sz = size(fld); allocate( ftmp(sz) )
-    ftmp = fld / rvals**scl
-    src(:) = -matmul(L0,fld)
-    src(:) = src(:) + rvals**scl*VPRIME(ftmp)
-    deallocate(ftmp)
-  end subroutine source_scaled
-
-  subroutine variation_scaled(fld,var,scl,rvals)
-    real(dl), dimension(1:), intent(in) :: fld
-    real(dl), dimension(1:,1:), intent(out) :: var
-    real(dl), intent(in) :: scl
-    real(dl), dimension(1:), intent(in) :: rvals
-    integer :: i, sz
-    real(dl), dimension(:), allocatable :: ftmp
-
-    sz = size(fld); allocate( ftmp(sz) )
-    ftmp = fld / rvals**scl
-
-    var(1:sz,1:sz) = L0(1:sz,1:sz)
-    do i=1,sz
-       ! Hmm, I think this multiplication by rvals on the V'' term is wrong.  Which means scaled and unscaled versions are the same
-       var(i,i) = var(i,i) - rvals(i)**scl*( VDPRIME(ftmp(i)) ) !- (3._dl*fld(i)**2-1._dl +2._dl*del*fld(i))
-    enddo
-    ! boundary condition at infinity
-    var(sz,:) = 0._dl
-    var(sz,sz) = 1._dl
-  end subroutine variation_scaled
 
   !>@brief
   !> A subroutine to set general Robin boundary conditions on our fields
